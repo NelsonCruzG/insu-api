e7697deaea1ec2390acb4b0e249b030d6fdc97b5
NelsonCruzG <nelsoncruz.1250@gmail.com>
2024-02-27 20:57:05 -0600

    feat: Create Auth & User modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { validationSchema } from './config';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [],
+  imports: [
+    AuthModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema,
+    }),
+    UsersModule,
+  ],
   controllers: [],
   providers: [],
 })
